14c8670d364e7ce9d3252d0f7d09bb10c8b2627a
mh <mh@localhost.localdomain>
2020-06-19 12:28:12 +0900

    detail display DB data

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.http import Http404
 from django.template import loader
 
 from .models import Question
@@ -9,16 +10,16 @@ def index(request):
     lastest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'lastest_question_list': lastest_question_list}
     return render(request, 'polls/index.html', context)
-    #lastest_question_list = Question.objects.order_by('-pub_date')[:5]
-    #template = loader.get_template('polls/index.html')
-    #context = {
-    #    'lastest_question_list': lastest_question_list,
-    #}
-    #return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question':question})
+    #try:
+    #    question = Question.objects.get(pk=question_id)
+    #except Question.DoesNotExist:
+    #    raise Http404("Question does not exist")
+    #return render(request, 'polls/detail.html', {'question':question})
 
 
 def results(request, question_id):
